e5ee4ac269cae78096a0e50a55dae438a5d77d92
Samrit Mukherjee <samritmukherjee05@gmail.com>
2026-03-07 03:27:18 +0530

    commit

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,1 +1,29 @@
-console.log('This is the api.ts file in the frontend/app/lib directory.');
+import admin from 'firebase-admin';
+
+const serviceAccount = {
+  type: 'service_account',
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: '118396032905414141452',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url: 'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40doubleslash-e9143.iam.gserviceaccount.com',
+  universe_domain: 'googleapis.com'
+};
+
+// Initialize Firebase Admin SDK if not already initialized
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  });
+}
+
+export const db = admin.firestore();
+export const storage = admin.storage();
+export const auth = admin.auth();
+
+export default admin;
